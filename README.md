3daaaddadbe65e6517a33c069011b0cce8778e5b
gunh0 <devgun@kakao.com>
2022-10-10 23:49:21 +0900

    feat: file scan & mv files

--- README.md
+++ README.md
@@ -3,9 +3,28 @@
 <br/>
 
 ### Input
-- Specify two directory paths
+
+-   Specify two directory paths
 
 <br/>
 
 ### Output
-- Scan and remove for each duplicate file
+
+-   Scan and remove for each duplicate file
+
+<br/>
+
+### Env.
+
+-   Python 3.9.7
+
+```
+# Make virtual env.
+python -m venv venv
+
+# Global variables
+REMOVE_PATH = "{root directory path to be deleted}"
+ORIGIN_PATH = "{source root directory path}"
+TRASH = "{duplicate Files}"
+NEED_CHECK = "{files that need to be verified}"
+```
---
+++ main.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import logging
+import os
+import shutil
+
+from filecmp import cmp
+
+logger = logging.getLogger(__name__)
+stream_handler = logging.StreamHandler()
+logger.setLevel(level=logging.INFO)
+logger.addHandler(stream_handler)
+file_handler = logging.FileHandler("./log.log")
+formatter = logging.Formatter("%(asctime)s : %(message)s")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+REMOVE_PATH = "C:\\Users\\gunho\\Desktop\\temp_176GB_v.22.9.15"
+ORIGIN_PATH = "D:\\__home"
+TRASH = "C:\\Users\\gunho\\Desktop\\Trash"
+NEED_CHECK = "C:\\Users\\gunho\\Desktop\\Check"
+
+check_num = 1
+
+
+def find_file(ORIGIN_PATH, remove_file_path):
+    for root, dirs, files in os.walk(ORIGIN_PATH):
+        for fname in files:
+            full_fname = os.path.join(root, fname)
+            if cmp(full_fname, remove_file_path):
+                logger.info("  [+] Match: " + str(full_fname))
+                shutil.move(remove_file_path, TRASH)
+                return True
+    return False
+
+
+for root, dirs, files in os.walk(REMOVE_PATH):
+    for fname in files:
+        full_fname = os.path.join(root, fname)
+        logger.info("[*][" + str(check_num) + "] Now: " + str(full_fname))
+        if find_file(ORIGIN_PATH, remove_file_path=full_fname):
+            print("=============== OK... Find! ===============")
+        else:
+            logger.info("  [-] Not Found!")
+            shutil.move(full_fname, NEED_CHECK)
+        check_num += 1
